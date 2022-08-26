7c6e1989ad91d6fd64da37d9e701686edb4d87d0
Dominik <53489780+DominikFischer86@users.noreply.github.com>
2022-08-26 02:34:21 +0200

    feat: Add player registration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {  useState } from "react"
 
-function App() {
+import "./App.css"
+
+const suits = ["diamonds", "clubs", "hearts", "spades"]
+const values = ["ace", "7", "8", "9", "T", "J", "Q", "K"]
+const initCards = []
+for (const s of suits) {
+  for (const v of values) {
+    initCards.push(`${v} of ${s}`)
+  }
+}
+
+const TIME_BANK = 3000
+const LIVES = 4
+const MAX_Players = 3
+const players = []
+const playerArray = ["1"]
+
+const App = () => {
+  const [playerCount, setPlayerCount] = useState(0)
+  const [player, setPlayer] = useState({
+    id: "0",
+    name: "",
+    dealer: true,
+    lives: LIVES,
+    timeBank: TIME_BANK,
+    position: 0
+  })
+
+  const [cards, setCards] = useState(
+    [...initCards].sort(() => Math.random() - 0.5)
+  )
+  const [communityCards, setCommunityCards] = []
+
+  const onChange = e => {
+    const targetId = e.target.name.split("_")[1]
+    const inputText = e.target.value
+
+    setPlayer({ 
+      ...player,
+      id: targetId,
+      name: inputText,
+      position: parseInt(targetId)
+    })
+  }
+
+  const addPlayer = e => {
+    e.preventDefault()
+    setPlayerCount(playerCount => playerCount + 1)
+    players.push(player)
+    playerArray.push(playerCount.toString)
+    setPlayer({ 
+      ...player,
+      id: playerCount.toString,
+      dealer: false,
+      name: "",
+      position: parseInt(playerCount)
+    })
+  }
+
+  const initGame = () => {
+    console.log(players)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Schwimmen</h1>
+      <hr />
+      <div className="getPlayersContainer">
+        {playerArray.map((_, id) => (
+            <form key={`player-${id}`} className="addPlayersForm" onSubmit={addPlayer}>
+              <input 
+                onChange={onChange} 
+                disabled={playerCount >= MAX_Players} 
+                type="text" 
+                id={`player_${playerCount}`}
+                name={`player_${playerCount}`}
+                placeholder={`Input name for player ${playerCount + 1}`}
+                value={players[playerCount]?.name} />
+              <button
+                disabled={playerCount >= MAX_Players || !player.name.length} 
+                onClick={addPlayer}>
+                  +
+              </button>
+            </form>
+          )
+        )}
+        <hr />
+        <button onClick={initGame}>Start game</button>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App
